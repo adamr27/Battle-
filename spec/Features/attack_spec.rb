4b92682253540b1d15b53540320a49bc52331aa3
adam robinson <adam.james.robinson@hotmail.co.uk>
2019-10-16 16:47:26 +0100

    extracting logic to the model

--- spec/Features/attack_spec.rb
+++ spec/Features/attack_spec.rb
@@ -4,4 +4,11 @@ feature 'Attacking' do
     click_button 'Attack'
     expect(page).to have_content 'Adam F attacked Adam R'
   end
+  scenario 'reduce Player 2 HP by 10' do
+    sign_in_and_play
+    click_button 'Attack'
+    click_link 'OK'
+    expect(page).not_to have_content 'Adam R: 60HP'
+    expect(page).to have_content 'Adam R: 50HP'
+  end
 end
---
+++ spec/player_spec.rb
@@ -0,0 +1,28 @@
+require 'player'
+
+describe Player do
+  subject(:adam_f) { Player.new('Adam F') }
+  subject(:adam_r) { Player.new('Adam R') }
+
+  describe '#name' do
+    it 'returns the name' do
+      expect(adam_f.name).to eq 'Adam F'
+    end
+  end
+  describe '#hitpoints' do
+    it 'returns the hit points' do
+      expect(adam_f.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+    end
+  end
+  describe '#attack' do
+    it 'damages the player' do
+      expect(adam_r).to receive(:receive_damage)
+      adam_f.attack(adam_r)
+    end
+  end
+  describe '#receive_damage' do
+    it 'reduces the player hit points' do
+      expect{adam_f.receive_damage}.to change { adam_f.hit_points }.by(-10)
+    end
+  end
+end
