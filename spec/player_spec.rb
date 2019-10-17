2142eef331ff655f4f3f375953336afecde7a876
adam robinson <adam.james.robinson@hotmail.co.uk>
2019-10-17 09:45:47 +0100

    Single Responsibility Principle, anyone?

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -14,12 +14,12 @@ describe Player do
       expect(adam_f.hit_points).to eq described_class::DEFAULT_HIT_POINTS
     end
   end
-  describe '#attack' do
-    it 'damages the player' do
-      expect(adam_r).to receive(:receive_damage)
-      adam_f.attack(adam_r)
-    end
-  end
+  # describe '#attack' do
+  #   it 'damages the player' do
+  #     expect(adam_r).to receive(:receive_damage)
+  #     adam_f.attack(adam_r)
+  #   end
+  # end
   describe '#receive_damage' do
     it 'reduces the player hit points' do
       expect{adam_f.receive_damage}.to change { adam_f.hit_points }.by(-10)
