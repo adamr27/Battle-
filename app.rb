82a53ede0cb6c3dcd3e3470a97540de92f4d33d6
adam robinson <adam.james.robinson@hotmail.co.uk>
2019-10-17 12:10:08 +0100

    all done

--- app.rb
+++ app.rb
@@ -12,22 +12,23 @@ class Battle < Sinatra::Base
   post '/names' do
     player_1 = Player.new(params[:player_1_name])
     player_2 = Player.new(params[:player_2_name])
-    $game = Game.new(player_1, player_2)
+    @game = Game.create(player_1, player_2)
     redirect '/play'
   end
 
+  before do
+    @game = Game.instance
+  end
+
   get '/play' do
-    @game = $game
     erb :play
   end
 
   get '/attack' do
-    @game = $game
     erb :attack
   end
 
   post '/attack' do
-    @game = $game
     @game.attack(@game.opponent_of(@game.current_turn))
     if @game.game_over?
       redirect '/game-over'
@@ -37,12 +38,11 @@ class Battle < Sinatra::Base
   end
 
   get '/game-over' do
-    @game = $game
     erb :game_over
   end
 
   post '/switch-turns' do
-    $game.switch_turns
+    @game.switch_turns
     redirect ('/play')
   end
 
