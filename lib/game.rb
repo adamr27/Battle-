92c2dc6967795f1d7a9c4ff6bb02dfba1e7e3687
Adam1824 <forrestadam07@gmail.com>
2019-10-17 10:34:15 +0100

    switching turns

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,34 @@
 class Game
+  attr_reader :current_turn
+  def initialize(player_1, player_2)
+    @players = [player_1, player_2]
+    @current_turn = player_1
+  end
+
+  def player_1
+    @players.first
+  end
+
+  def player_2
+    @players.last
+  end
+
   def attack(player)
     player.receive_damage
+  end
+
+  def switch_turns
+    @current_turn = opponent_of(current_turn)
+  end
+
+  private
+
+  def opponent_of(the_player)
+    #@players.select { |player| player != the_player }.first
+    if the_player == player_1
+      player_2
+    else
+      player_1
+    end
   end
 end
