64c150c8516e81fd5ad0f0fcef550b60f2cb0d8b
Adam1824 <forrestadam07@gmail.com>
2019-10-17 16:20:21 +0100

    completed fully first extension user story

--- spec/Features/attack_spec.rb
+++ spec/Features/attack_spec.rb
@@ -6,24 +6,20 @@ feature 'Attacking' do
   end
   scenario 'reduce Player 2 HP by a random amount' do
     sign_in_and_play
-    click_button 'Attack'
-    click_button 'OK'
+    attack_and_confirm
     expect(page).not_to have_content 'Adam R: 60HP'
   end
 
   scenario 'be attacked by Player 2' do
     sign_in_and_play
-    click_button 'Attack'
-    click_button 'OK'
+    attack_and_confirm
     click_button 'Attack'
     expect(page).to have_content 'Adam R attacked Adam F'
   end
   scenario 'reduce Player 1 HP by a random amount' do
     sign_in_and_play
-    click_button 'Attack'
-    click_button 'OK'
-    click_button 'Attack'
-    click_button 'OK'
+    attack_and_confirm
+    attack_and_confirm
     expect(page).not_to have_content 'Adam F: 60HP'
   end
 end
--- spec/Features/game_over_spec.rb
+++ spec/Features/game_over_spec.rb
@@ -1,14 +1,15 @@
-
-
 feature 'Game over' do
   context 'when Player 1 reaches 0 HP first' do
-    before do
-      until page == '/game-over' do
-        attack_and_confirm
-      end
-    end
-
     scenario 'Player 2 loses' do
+      sign_in_and_play
+      # until page.current_path == '/game-over' do
+      #   attack_and_confirm
+      # end
+      loop do
+        click_button 'Attack'
+        break if page.current_path == '/game-over'
+        click_button 'OK'
+      end
       expect(page).to have_content 'loses!'
     end
   end
